724967bfcea92c2d62b6b5c83fa3b0ed92dba50c
theavalanch <jasonfreakinevans11@gmail.com>
2024-11-13 10:26:56 +0100

    list

---
+++ fff.py
@@ -0,0 +1,35 @@
+text = "hello world!"
+print(text)
+
+# things = ["apple","ball", "car","table", "cow"]
+# while len(things):
+#     x=0
+#     while x < len(things):
+#         print(things[x])
+#         x+=1
+    # print((things))
+    # print((things))
+
+
+# PSEUDO CODE
+# Create a list of items
+# Create a starting point for iteration
+# wjile starting point is still less than the length of list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
